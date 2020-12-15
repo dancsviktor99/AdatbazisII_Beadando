5f5f0800ea2ed5b2e575fbb7506d8346263f57db
dancsviktor99 <55428134+dancsviktor99@users.noreply.github.com>
2020-12-16 00:24:41 +0100

    Property-k létrehozása, SELECT játékok lista
    Összes property létrehozása, egyenlőre beépített kivételekkel.
    Select feltöltése
    -OracleCommand
    -OracleDataReader

---
+++ DBBeadando/DBBeadando/Models/Manager/KaszinoJatekokTabla.cs
@@ -0,0 +1,51 @@
+﻿using DBBeadando.Models.Records;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBBeadando.Models.Manager
+{
+    class KaszinoJatekokTabla
+    {
+        OracleConnection GetOracleConnection()
+        {
+            OracleConnection oc = new OracleConnection();
+
+            string connectionString = @"Data Source=193.225.33.71;User Id=Q4NGPR;Password=EKE2020;";
+            oc.ConnectionString = connectionString;
+            return oc;
+        }
+
+        public List<Jatekok> Select()
+        {
+            List<Jatekok> records = new List<Jatekok>();
+            OracleConnection oc = new OracleConnection();
+            oc.Open();
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.Text,
+                CommandText = "SELECT k.nev, j.asztalkod, j.jatekasztalneve,j.gyarto,j.asztallimit,j.asztal_datuma,j.jatektipusa FROM jatekok j INNER JOIN krupiek i ON k_id = j.krupie_id"
+            };
+            command.Connection = oc;
+
+            OracleDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Jatekok jatek = new Jatekok();
+                jatek.Asztalkod = reader["asztalkod"].ToString();
+                jatek.Jatekasztalneve = reader["jatekasztalneve"].ToString();
+                jatek.Gyarto = reader["gyarto"].ToString();
+                jatek.Jatektipusa = reader["jatektipusa"].ToString();
+
+                records.Add(jatek);
+            }
+            oc.Close();
+
+            return records;
+        }
+    }
+}
---
+++ DBBeadando/DBBeadando/Models/Records/Jatekok.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBBeadando.Models.Records
+{
+    public class Jatekok
+    {
+        private string asztalkod;
+
+        public string Asztalkod
+        {
+            get { return asztalkod; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Az asztalkód nem lehet nulla!");
+                }
+                asztalkod = value; 
+            }
+        }
+        private string jatekasztalneve;
+
+        public string Jatekasztalneve
+        {
+            get { return jatekasztalneve; }
+            set { jatekasztalneve = value; }
+        }
+
+        private string gyarto;
+
+        public string Gyarto
+        {
+            get { return gyarto; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("A gyártó nem lehet nulla!");
+                }
+                gyarto = value; 
+            }
+        }
+
+        private int asztallimit;
+
+        public int Asztallimit
+        {
+            get { return asztallimit; }
+            set 
+            {
+                if (value <=  0 || value > 100000)
+                {
+                    throw new ArgumentException("Nem lehet nulla és negatív, továbbá 100.000 felett!");
+                }
+                asztallimit = value; 
+            }
+        }
+
+        private string krupie;
+
+        public string Krupie
+        {
+            get { return krupie; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("A Krupie neve nem lehet üres!");
+                }
+                krupie = value; 
+            }
+        }
+
+        private int asztal_datuma;
+
+        public int Asztal_datuma
+        {
+            get { return asztal_datuma; }
+            set { asztal_datuma = value; }
+        }
+
+        private string jatektipusa;
+                    
+        public string Jatektipusa
+        {
+            get { return jatektipusa; }
+            set { jatektipusa = value; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
